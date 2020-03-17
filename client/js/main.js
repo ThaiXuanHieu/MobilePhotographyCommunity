5404eaf507469254951dd7e4fafd6d6086a5188e
ThaiXuanHieu <48479522+ThaiXuanHieu@users.noreply.github.com>
2020-03-17 18:20:08 +0700

    update

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,15 @@
 $('#upload-img').filestyle({
     input: false,
     iconName: 'glyphicon glyphicon-picture',
-    buttonText: 'Thêm ảnh'
+    buttonText: 'Thêm ảnh',
+});
+
+$('#upload-img').change(function(e){
+    // var path = $('#upload-img').val();
+    // if (path.substring(3,11) == 'fakepath') {
+    //     path = path.substring(12);
+    // } // Remove c:\fake at beginning from localhost chrome
+    var location = URL.createObjectURL(e.target.files[0]);
+    $('.post-img-preview').attr('src', location);
+    $('#txtPath').val(location);
 });
